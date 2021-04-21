1e88a0bcec5315eb86789ab01ae0e2846a414583
Trevor White <trevor.white@wayne.edu>
2021-04-21 00:51:34 -0400

    Create cryptoportfoliotracker.bat
    Easily run on windows by using batch file

---
+++ cryptoportfoliotracker.bat
@@ -0,0 +1,3 @@
+cd C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker
+C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker\venv\Scripts\python.exe "C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker\cryptoportfoliotracker.py"
+pause
