4f4a6db1d03167a13d540332137fc02bbbb00e9e
Trevor White <trevor.white@wayne.edu>
2021-04-21 02:51:27 -0400

    Added total to cli, added kill background firefox processes upon keyboard interrupt, added demo gif

--- cli.py
+++ cli.py
@@ -4,6 +4,7 @@ from tabulate import tabulate
 
 
 def print_data(coins):
+    total = 0
     coin_data_dict = {
         'Token': [],
         'Price': [],
@@ -15,12 +16,18 @@ def print_data(coins):
         coin_data_dict["Token"].append(coin.symbol)
         coin_data_dict["Price"].append(coin.price)
         coin_data_dict["Quantity"].append(coin.quantity)
-        coin_data_dict["Balance"].append(coin.balance)
+        coin_data_dict["Balance"].append(f'${coin.balance}')
+        total += float(coin.balance)
+    # add total as last row in CLI
+    coin_data_dict["Token"].append("TOTAL")
+    coin_data_dict["Price"].append("-")
+    coin_data_dict["Quantity"].append("-")
+    coin_data_dict["Balance"].append(f'${total:.2f}')
     # convert coin_data_dictionary to pandas dataframe
     df = pd.DataFrame(coin_data_dict)
     _cls()
-    return print(tabulate(df, headers='keys', tablefmt='psql'))
+    return print(tabulate(df, headers='keys', tablefmt='fancy_grid'))
 
 
 def _cls():
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
--- cryptoportfoliotracker.py
+++ cryptoportfoliotracker.py
@@ -1,4 +1,5 @@
-from os import getcwd, path
+import sys
+from os import getcwd, path, system
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
@@ -40,30 +41,45 @@ def get_balance(coin):
     return f'{coin.price * coin.quantity:.2f}'
 
 
-def main():
-    base_url = "https://charts.bogged.finance/?token="
-    coins = get_coins()
-    # coin actions that only need to run once
-    for coin in coins:
-        coin.set_driver(get_driver())
-        coin.driver.get(base_url + coin.contract)
+def terminate():
+    system("taskkill /im firefox.exe /f")
+    print("Sucessfully terminated all Firefox processes.\nSafe to exit. ")
 
-    # coin actions that update in infinite loop
-    while True:
+def main():
+    coins = None
+    try:
+        cli._cls()
+        print("Gathering coin data... this might take a few seconds.")
+        print("\n\n\n\n\n\n\n\n\n\nPress CTRL+C to terminate safely.")
+        base_url = "https://charts.bogged.finance/?token="
+        coins = get_coins()
+        # coin actions that only need to run once
         for coin in coins:
-            # update each coin's dynamic data
-            while True:
-                # check if browser is loaded
-                try:
-                    page_data = BeautifulSoup(coin.driver.page_source, 'html.parser')
-                    coin.set_symbol(get_symbol(page_data))
-                    coin.set_price(get_price(page_data))
-                    coin.set_balance(get_balance(coin))
-                    break
-                # try again if browser not loaded
-                except:
-                    continue
-        cli.print_data(coins)
+            coin.set_driver(get_driver())
+            coin.driver.get(base_url + coin.contract)
+
+        # coin actions that update in infinite loop
+        while True:
+            for coin in coins:
+                # update each coin's dynamic data
+                while True:
+                    # check if browser is loaded
+                    try:
+                        page_data = BeautifulSoup(coin.driver.page_source, 'html.parser')
+                        coin.set_symbol(get_symbol(page_data))
+                        coin.set_price(get_price(page_data))
+                        coin.set_balance(get_balance(coin))
+                        break
+                    # try again if browser not loaded
+                    except KeyboardInterrupt:
+                        terminate()
+                    except:
+                        continue
+            cli.print_data(coins)
+            print("\n\n\nPress CTRL+C to terminate safely.")
+    except KeyboardInterrupt:
+        terminate(coins)
+
 
 if __name__ == '__main__':
     main()
