530416f5f13fa9c95c9a72906ba28dc11ab0544c
Trevor White <trevor.white@wayne.edu>
2023-10-11 07:49:30 -0400

    Cleaned up repo.

--- cryptoportfoliotracker.bat
+++
@@ -1,3 +0,0 @@
-cd C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker
-C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker\venv\Scripts\python.exe "C:\Users\trevo\Documents\GitHub\CryptoPortfolioTracker\cryptoportfoliotracker.py"
-pause
